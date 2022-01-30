08b403969432e5ce6f337fdd2f7857d6ddb148c7
Rodrigo Moreira <hola@piducan.dev>
2022-01-30 15:59:13 -0300

    force webpack require firebase

---
+++ gatsby-node.js
@@ -0,0 +1,17 @@
+// force Webpack to import Firebase with the require method
+// https://github.com/firebase/firebase-js-sdk/issues/2222#issuecomment-538072948
+
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      externals: getConfig().externals.concat(function (context, request, callback) {
+        const regex = /^@?firebase(\/(.+))?/;
+        // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
+        if (regex.test(request)) {
+          return callback(null, "umd " + request);
+        }
+        callback();
+      }),
+    });
+  }
+};
